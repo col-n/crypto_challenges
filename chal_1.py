52c5028ca2c9ff84134906738b00ffacdb39aad0
Colin <colin.ahern@gmail.com>
2016-10-22 12:59:14 -0400

    finished cha 1

--- chal_1.py
+++ chal_1.py
@@ -1,2 +1,18 @@
 #challenge 1
-print('hello world')
+import binascii
+import base64
+
+def hex_64(i):
+	decoder = binascii.unhexlify(x)
+	return base64.b64encode(decoder).decode('ascii')
+
+x = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+
+should_y = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+y = hex_64(x)
+
+if y == should_y:
+	print('yay!')
+else:
+	print('dang!')
